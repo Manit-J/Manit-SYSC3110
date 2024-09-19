2501a8355a78b7a880a2a0ce55e2fe7c40b6a9f8
Manit <manitjawa@gmail.com>
2024-09-19 15:16:16 -0400

    Added the main method

---
+++ src/AddressBook.java
@@ -0,0 +1,17 @@
+public class AddressBook {
+
+    public AddressBook() {
+    }
+
+    public void addBuddy() {
+
+    }
+
+    public void removeBuddy(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
