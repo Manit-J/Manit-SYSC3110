92c44a83e520e0e8853d1c3af3c57ecd00c94821
Manit <manitjawa@gmail.com>
2024-09-19 15:28:26 -0400

    Added the add, remove methods and changed the main method

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,17 +1,26 @@
+import java.util.*;
+
 public class AddressBook {
 
+    private HashMap<String, String> addressBook;
+
     public AddressBook() {
+        addressBook = new HashMap<String, String>();
     }
 
-    public void addBuddy() {
-
+    public void addBuddy(BuddyInfo buddy) {
+        addressBook.put(buddy.getName(), buddy.getAddress());
     }
 
-    public void removeBuddy(){
-
+    public void removeBuddy(BuddyInfo buddy){
+        addressBook.remove(buddy.getName());
     }
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo myBuddy = new BuddyInfo("John", "Ottawa", "8989891881");
+        AddressBook myBook = new AddressBook();
+        myBook.addBuddy(myBuddy);
+        myBook.removeBuddy(myBuddy);
     }
 }
